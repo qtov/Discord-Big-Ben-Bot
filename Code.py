e0c8e9f968cbea39bbdc97a805ed645979aabef6
Paul-Mihai Forgaci <tigrex0806@gmail.com>
2024-09-27 20:40:56 +0000

    do simple updates

--- Code.py
+++ Code.py
@@ -1,12 +1,12 @@
+import os
+import pytz
+import asyncio
 import discord
 from discord.ext import commands, tasks
-import asyncio
 from datetime import datetime
-import pytz
 from dotenv import load_dotenv
-import os
 
-load_dotenv('environment/var.env')
+load_dotenv('.env')
 
 intents = discord.Intents.default()
 intents.members = True
@@ -23,11 +23,51 @@ async def on_ready():
         return
 
     print(f"Connected to server: {bot.guilds[0].name} - Targeting VC-{VOICE_CHANNEL_NAME}")
-    hourly_bong.start()
+    check_hourly_bong.start()
     print("Hourly bong task started.")
 
+
+async def bong(now):
+    print(f"Time is {now.strftime('%H:%M')} - on the hour! Preparing to bong...")
+    current_hour = now.hour % 12
+    if not current_hour:
+        current_hour = 12
+    print(f"Current hour: {current_hour}{'PM' if now.hour >= 12 else 'AM'}")
+
+    guild = bot.guilds[0]
+    voice_channel = discord.utils.get(guild.voice_channels, name=VOICE_CHANNEL_NAME)
+
+    if not voice_channel:
+        print(f"Voice channel '{VOICE_CHANNEL_NAME}' not found.")
+        return
+
+    non_bot_members = [member for member in voice_channel.members if not member.bot]
+    if len(non_bot_members) == 0:
+        print(f"No members in the voice channel '{voice_channel.name}'. Skipping bong.")
+        return
+
+    if bot.voice_clients:
+        print("Bot is already connected to a voice channel.")
+        return
+
+    try:
+        vc = await voice_channel.connect()
+        print(f"Connected to the voice channel: {voice_channel.name}")
+        for count in range(current_hour):
+            print(f"Playing bong {count + 1} of {current_hour}...")
+            vc.play(discord.FFmpegPCMAudio(os.getenv('WAV_PATH')))
+            while vc.is_playing():
+                await asyncio.sleep(2)
+
+        print("All bongs played. Disconnecting...")
+        await vc.disconnect()
+        print("Disconnected from the voice channel.")
+    except Exception as e:
+        print(f"Error while connecting or playing audio: {e}")
+
+
 @tasks.loop(minutes=1)
-async def hourly_bong():
+async def check_hourly_bong():
     timezone_str = os.getenv('TIMEZONE', 'Europe/London')
     try:
         timezone = pytz.timezone(timezone_str)
@@ -37,41 +77,11 @@ async def hourly_bong():
 
     now = datetime.now(timezone)
 
-    if now.minute == 0:
-        print(f"Time is {now.strftime('%H:%M')} - on the hour! Preparing to bong...")
-        current_hour = now.hour
-        print(f"Current hour: {current_hour}")
+    # Return early if time is not ok.
+    if now.minute != 0:
+        return
 
-        guild = bot.guilds[0]
-        voice_channel = discord.utils.get(guild.voice_channels, name=VOICE_CHANNEL_NAME)
-
-        if not voice_channel:
-            print(f"Voice channel '{VOICE_CHANNEL_NAME}' not found.")
-            return
-
-        non_bot_members = [member for member in voice_channel.members if not member.bot]
-        if len(non_bot_members) == 0:
-            print(f"No members in the voice channel '{voice_channel.name}'. Skipping bong.")
-            return
-
-        if not bot.voice_clients:
-            try:
-                vc = await voice_channel.connect()
-                print(f"Connected to the voice channel: {voice_channel.name}")
-                for count in range(current_hour):
-                    print(f"Playing bong {count + 1} of {current_hour}...")
-                    vc.play(discord.FFmpegPCMAudio(os.getenv('WAV_PATH')))
-                    while vc.is_playing():
-                        await asyncio.sleep(1)
-
-                print("All bongs played. Disconnecting...")
-                await vc.disconnect()
-                print("Disconnected from the voice channel.")
-                
-            except Exception as e:
-                print(f"Error while connecting or playing audio: {e}")
-        else:
-            print("Bot is already connected to a voice channel.")
+    await bong(now)
 
 token = os.getenv("DISCORD_TOKEN")
 print(f"Token: {token}")
--- README.md
+++ README.md
@@ -2,7 +2,18 @@
 A bot that will join a set voice channel and play the sound of big ben's bong for every hour it is.
 
 ## Setup
-[Click Here for local installation guide](https://youtu.be/A-zSmLF5uzA)
+```
+# Setup the env:
+echo DISCORD_TOKEN=<yourdiscordtoken> > .env
+echo VOICE_CHANNEL_NAME=<channel_to_join> >> .env
+echo TIMEZONE=Europe/Bucharest >> .env
+echo WAV_PATH=big_ben_bell.wav >> .env
+
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python3 Code.py
+```
 
 ## Function
 The bot will join a pre-defined (In environment/env.var) voice channel at the start of every hour xx:00. It will then play the wav file once for every hour it is (1am one play, 11pm, 23 plays etc).
